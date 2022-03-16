ee5025b1a9f1db9684fe45c9f571b2f9b7632a56
munhong,KIM <93499222+iwipwq@users.noreply.github.com>
2022-03-16 23:45:42 +0900

    add header component
    add header component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
+import Header from './components/Header';
 import AuthContextProvider from './context/AuthContext';
 
 function App() {
   return (
     <AuthContextProvider>
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
